dc9223247fb11a3db3107d8812e53f6721a2c0a1
jayden220 <jaydenvanrooyen87@gmail.com>
2024-08-29 14:52:38 +0200

    created backend

---
+++ controlller/ProductController.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { products } from '../model/index.js'
+const productRouter = express.Router()
+productRouter.use(bodyParser.json())
+productRouter.get('/', (req, res) => {
+    products.getProducts(req, res)
+})
+productRouter.get('/:id', (req, res) =>{
+    products.getProduct(req, res)
+})
+productRouter.post('/addProduct', (req, res) => {
+    products.addProduct(req, res)
+})
+productRouter.patch('/update/:id', (req, res) => {
+    products.editProduct(req, res)
+})
+productRouter.delete('/delete/:id', (req, res) => {
+    products.deleteProduct(req, res)
+})
+export {
+    productRouter
+}
