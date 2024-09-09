73f5580de03fdb1b18a1f59772c8b71f97024a9e
jayden220 <jaydenvanrooyen87@gmail.com>
2024-09-09 15:55:00 +0200

    add cart

--- model/cartDb.js
+++ model/cartDb.js
@@ -1,36 +1,34 @@
-import {pool} from '../config/config.js'
+import { pool } from '../config/config.js';
 
-const fetchCarts = (req, res) => {
+const fetchCarts = async (req, res) => {
   try {
     const strQry = `
-      SELECT distinct c.userID, CONCAT(u.userName) AS fullName,
+      SELECT distinct c.userID, u.userName AS fullName,
           group_concat(DISTINCT p.productName) 'Product',
           sum(p.productPrice * c.quantity)  'Total Price',
           sum(c.quantity) 'Quantity'
       FROM Carts c
       JOIN users u ON c.userID = u.userID
-      JOIN products p ON c.prodID = p.productID
+      JOIN products p ON c.productID = p.productID
       group by c.userID;
-    `
-    pool.query(strQry, (err, results) => {
-      if (err) throw new Error('Unable to fetch all carts')
-      res.json({
-        status: res.statusCode,
-        results
-      })
-    })
+    `;
+    const [results] = await pool.query(strQry);
+    res.json({
+      status: res.statusCode,
+      results
+    });
   } catch (e) {
     res.json({
       status: 404,
       msg: e.message
-    })
+    });
   }
 }
 
-const fetchuserCart = (req, res) => {
+const fetchuserCart = async (req, res) => {
   try {
     const strQry = `
-      SELECT c.userID, CONCAT(u.userName) AS fullName,
+      SELECT c.userID, u.userName AS fullName,
           p.productID,
           p.productName 'Product',
           p.productPrice * c.quantity  'Total Price',
@@ -39,114 +37,124 @@ const fetchuserCart = (req, res) => {
       JOIN users u ON c.userID = u.userID
       JOIN products p ON c.productID = p.productID
       WHERE c.userID = ?;
-    `
-    pool.query(strQry, [req.params.id], (err, results) => {
-      if (err) throw new Error(err)
-      res.json({
-        status: res.statusCode,
-        results
-      })
-    })
+    `;
+    const [results] = await pool.query(strQry, [req.params.id]);
+    res.json({
+      status: res.statusCode,
+      results
+    });
   } catch (e) {
     res.json({
       status: 404,
       msg: e.message
-    })
+    });
   }
 }
 
-const fetchaddUserCart = (req, res) => {
+const fetchadduserCart = async (req, res) => {
   try {
     const strQry = `
       INSERT INTO Carts
       (userID, productID, quantity)
       VALUES (?, ?, ?);
-    `
-    pool.query(strQry, [req.params.id, req.body.productID, req.body.quantity], (err, results) => {
-      if (err) throw new Error(err)
-      res.json({
-        status: res.statusCode,
-        msg: 'added to cart '
-      })
-    })
+    `;
+    await pool.query(strQry, [req.params.id, req.body.productID, req.body.quantity]);
+    res.json({
+      status: res.statusCode,
+      msg: 'Added to cart'
+    });
   } catch (e) {
     res.json({
       status: 404,
       msg: e.message
-    })
+    });
   }
 }
 
-const fetchupdateUserCart = (req, res) => {
+const fetchupdateUserCart = async (req, res) => {
   try {
     const strQry = `
       UPDATE Carts
       SET quantity = ?
       WHERE productID = ? AND userID = ?;
-    `
-    pool.query(strQry, [req.body.quantity, req.params.productID, req.params.id], (err, results) => {
-      if (err) throw new Error('Unable to update cart')
+    `;
+    const [result] = await pool.query(strQry, [req.body.quantity, req.params.productID, req.params.id]);
+    if (result.affectedRows > 0) {
       res.json({
         status: res.statusCode,
         msg: 'The quantity has been updated. Aragato:grin:'
-      })
-    })
+      });
+    } else {
+      res.json({
+        status: 404,
+        msg: 'Cart item not found'
+      });
+    }
   } catch (e) {
     res.json({
       status: 404,
       err: e.message
-    })
+    });
   }
 }
 
-const deleteItem = (req, res) => {
+const deleteItem = async (req, res) => {
   try {
     const strQry = `
       DELETE FROM Carts
       WHERE productID = ? AND userID = ?;
-    `
-    pool.query(strQry, [req.params.productID, req.params.id], (err) => {
-      if (err) throw new Error('Unable to delete item')
+    `;
+    const [result] = await pool.query(strQry, [req.params.productID, req.params.id]);
+    if (result.affectedRows > 0) {
       res.json({
         status: res.statusCode,
         msg: 'An item was removed'
-      })
-    })
+      });
+    } else {
+      res.json({
+        status: 404,
+        msg: 'Item not found'
+      });
+    }
   } catch (e) {
     res.json({
       status: 404,
       err: e.message
-    })
+    });
   }
 }
 
-const deleteCart = (req, res) => {
+const deleteCart = async (req, res) => {
   try {
     const strQry = `
       DELETE FROM Carts
       WHERE userID = ?;
-    `
-    pool.query(strQry, [req.params.id], (err) => {
-      if (err) throw new Error('Unable to delete this Cart')
+    `;
+    const [result] = await pool.query(strQry, [req.params.id]);
+    if (result.affectedRows > 0) {
       res.json({
         status: res.statusCode,
         msg: 'A cart was removed'
-      })
-    })
+      });
+    } else {
+      res.json({
+        status: 404,
+        msg: 'Cart not found'
+      });
+    }
   } catch (e) {
     res.json({
       status: 404,
       err: e.message
-    })
+    });
   }
 }
 
 export {
   fetchCarts,
   fetchuserCart,
-  fetchaddUserCart,
+  fetchadduserCart,
   fetchupdateUserCart,
   deleteItem,
   deleteCart
-
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { getUsers,getUser,addUser,deleteUser,updateUser } from '../controller/userController.js';
 import { verifyAToken,checkUser } from '../middleware/authenticate.js';
 
-import { fetchCarts, fetchuserCart, fetchaddUserCart, fetchupdateUserCart, deleteItem, deleteCart } from '../model/cartDb.js';
+import { fetchCarts, fetchuserCart, fetchadduserCart, fetchupdateUserCart, deleteItem, deleteCart } from '../model/cartDb.js';
 
 // const express = require('express')
 const userRouter = express.Router()
@@ -11,16 +11,16 @@ const userRouter = express.Router()
 userRouter.use(bodyParser.json());
 
 userRouter.get('/', getUsers)
-userRouter.get('/:id', getUser)
+userRouter.get('/singleUser/:id', getUser)
 userRouter.post('/register',addUser)
-userRouter.patch('/update/:id',updateUser)
-userRouter.delete('/delete/:id',deleteUser)
+userRouter.patch('/:id',updateUser)
+userRouter.delete('/:id',deleteUser)
 userRouter.post('/login', verifyAToken,checkUser)
 userRouter.post('/logout')
 
 userRouter.get('/carts', fetchCarts ) 
 userRouter.get('/:id/cart',fetchuserCart )
-userRouter.post('/:id/cart', fetchaddUserCart ) 
+userRouter.post('/:id/cart', fetchadduserCart ) 
 userRouter.patch('/:id/cart/:productID', fetchupdateUserCart)
 userRouter.delete('/:id/cart', deleteCart) 
 userRouter.delete('/:id/cart/:productID', deleteItem)
