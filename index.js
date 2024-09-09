ad0519abf5f0694c67af89fe463b1432080447ae
jayden220 <jaydenvanrooyen87@gmail.com>
2024-09-09 11:03:42 +0200

    controller changes

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ const port = 3000
 app.use(express.json())
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials:true
 }))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
 
 app.use(express.static('public'))
 app.use('/product',productRouter)
